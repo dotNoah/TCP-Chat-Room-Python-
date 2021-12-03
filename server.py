88a89a2e2901f5475fc4e434d0308d4de5fb3cb3
dotNoah <78569452+dotNoah@users.noreply.github.com>
2021-12-02 23:24:12 -0600

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import threading
 import socket
 host = '127.0.0.1'
-port = 59000
+port = 2024
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
